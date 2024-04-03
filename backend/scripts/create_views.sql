184b9b39e2c0fe43dd60931f1b492e02538bda04
Charley Liu <charleyliu716@gmail.com>
2024-04-03 18:56:20 -0400

    implemented views

--- backend/scripts/create_views.sql
+++ backend/scripts/create_views.sql
@@ -1,4 +1,6 @@
 -- view 1
+DROP VIEW IF EXISTS area;
+
 Create VIEW area AS
 SELECT h.city,
     h.region,
@@ -9,8 +11,12 @@ GROUP BY h.city, h.region
 ORDER BY h.city, h.region;
 
 -- view 2
+DROP VIEW IF EXISTS totalcapacity;
+
 CREATE VIEW totalcapacity AS
 SELECT hotel.hotel_id,
+       hotel.hotel_chain_name,
+       hotel.street,
     sum(room.capacity) AS total_capacity
 FROM hotel
     JOIN room ON hotel.hotel_id = room.hotel_id
--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,9 +1,23 @@
-import { Input, Typography, Button } from "@material-tailwind/react";
+import { Card, CardBody, Input, Typography, Button } from "@material-tailwind/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Root({ customerUI, user, setUser }) {
     const [formData, setFormData] = useState({ ssn: '', isCustomer: customerUI });
+    const [hotelsPerArea, setHotelsPerArea] = useState(null);  // view 1
+    const [hotelsCapacity, setHotelsCapacity] = useState(null);  // view 2
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response1 = await axios.get('http://localhost:3000/api/hotels/area');
+            setHotelsPerArea(response1.data);
+
+            const response2 = await axios.get('http://localhost:3000/api/hotels/capacity');
+            setHotelsCapacity(response2.data);
+        };
+
+        fetchData();
+    }, []);
 
     useEffect(() => {
         setFormData({ ...formData, isCustomer: customerUI });
@@ -41,7 +55,7 @@ function Root({ customerUI, user, setUser }) {
     }
 
     return (
-        <div className="text-center my-8">
+        <div className="text-center my-8 mx-40">
             { user === null ? 
                 <form className="w-96 mx-auto" onSubmit={handleSubmit}>
                     <Typography>
@@ -64,7 +78,7 @@ function Root({ customerUI, user, setUser }) {
                 </form>
                 :
                 <>
-                    <div className="mb-3">
+                    <div className="mb-12">
                         <Typography variant="h2" className="mb-2">
                             E-Hotels
                         </Typography>
@@ -79,6 +93,44 @@ function Root({ customerUI, user, setUser }) {
                                 In order to book a room from our partnered hotels, click on the Book link in the navbar.
                             </Typography>
                         }
+                    </div>
+                    <div className="mb-12">
+                        <Typography variant="h4" className="mb-2">
+                            Number of Hotels per Area
+                        </Typography>
+                        <div className="my-8 flex flex-wrap gap-x-6 gap-y-5 justify-begin">
+                            { hotelsPerArea !== null && hotelsPerArea.map((row, index) => (
+                                <Card key={index} className="text-left bg-gray-300 p-3">
+                                    <Typography variant="h5" color="blue-gray" className="mb-4">
+                                        { row.city + ", " + row.region }
+                                    </Typography>
+                                    <CardBody>
+                                        <Typography className="text-black">
+                                            Rooms: { row.rooms }
+                                        </Typography>
+                                    </CardBody>
+                                </Card>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="mb-12">
+                        <Typography variant="h4" className="mb-2">
+                            Hotel Capacity
+                        </Typography>
+                        <div className="my-8 flex flex-wrap gap-x-6 gap-y-5 justify-begin">
+                            { hotelsCapacity !== null && hotelsCapacity.map((row, index) => (
+                                <Card key={index} className="text-left bg-gray-300 p-3">
+                                    <Typography variant="h5" color="blue-gray" className="mb-4">
+                                        { row.hotel_chain_name + ": " + row.street }
+                                    </Typography>
+                                    <CardBody>
+                                        <Typography className="text-black">
+                                            Capacity: { row.total_capacity }
+                                        </Typography>
+                                    </CardBody>
+                                </Card>
+                            ))}
+                        </div>
                     </div>
                     <div>
                         <Button className="mt-3 bg-red-500" onClick={handleLogout}>
