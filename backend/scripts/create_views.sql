74ee89e9900e8002f9e4b5913eb4bd312fe1b913
Lucas Gavura <lgavu074@uottawa.ca>
2024-04-03 22:50:46 -0400

    fixed first view

--- backend/scripts/create_views.sql
+++ backend/scripts/create_views.sql
@@ -2,13 +2,23 @@
 DROP VIEW IF EXISTS area;
 
 Create VIEW area AS
+WITH validrooms AS (
+    SELECT room.hotel_id,
+        count(room.room_id) AS valid_rooms_count
+    FROM room
+    LEFT JOIN archive ON room.room_id = archive.room_id 
+        AND archive.start_date <= CURRENT_DATE 
+        AND CURRENT_DATE <= archive.end_date
+    WHERE archive.room_id IS NULL
+    GROUP BY room.hotel_id
+)
 SELECT h.city,
     h.region,
-    count(r.room_id) AS rooms
+    COALESCE(validrooms.valid_rooms_count, 0::bigint) AS rooms
 FROM hotel h
-    LEFT JOIN room r ON h.hotel_id = r.hotel_id
-GROUP BY h.city, h.region
+LEFT JOIN validrooms ON h.hotel_id = validrooms.hotel_id
 ORDER BY h.city, h.region;
+
 
 -- view 2
 DROP VIEW IF EXISTS totalcapacity;
