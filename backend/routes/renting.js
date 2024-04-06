e8ed74481baa55857dc6183bc404e3cc4f5b4b3b
DonkAHez <88563086+DonkAHez@users.noreply.github.com>
2024-04-05 22:09:23 -0400

    create renting

--- backend/routes/renting.js
+++ backend/routes/renting.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.put('/', async (req, res) => {
+    
     try {
         let query = `INSERT INTO Archive (room_id, customer_SSN, employee_SSN, type, start_date, end_date) VALUES
-            (${req.body.room_id}, '${req.body.customer_ssn}', '${req.body.emplyoee_ssn}', 'renting', '${req.body.start_date}', '${req.body.end_date}'`;
+            (${req.body.room_id}, '${req.body.customer_ssn}', '${req.body.employee_ssn}', 'renting', '${req.body.start_date}', '${req.body.end_date}');`;
 
         const result = await req.pgClient.query(query);
         res.json(result.rows);
