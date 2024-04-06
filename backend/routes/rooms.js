3864b8f4dad61f547d1771110f2511c3c70a1474
Lucas Gavura <lgavu074@uottawa.ca>
2024-04-05 21:56:31 -0400

    working hotels UI

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const query = 'SELECT * FROM Room';
+        const result = await req.pgClient.query(query);
+        const rooms = result.rows;
+        res.json(rooms);
+    } catch(err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 router.get('/search', async (req, res) => {
     try {
         let query =`WITH ValidRooms AS (
